578ca42c0609ddfddb2636933ca682c45348172d
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2025-04-19 14:45:58 +0300

    a721dbe

---
+++ _nuxt/CPr5lVV4.js
@@ -0,0 +1,1 @@
+import{i as a,k as n,o as s,w as f,y as p,e as _}from"./mKrHTUBj.js";const i=a({__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(t){const e=t;return(r,c)=>{const o=_;return s(),n(o,{href:e.href,target:e.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}});export{i as default};
